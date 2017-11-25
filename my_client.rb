c48e1e34b66a8287af4e577708c0ddc4bcbf4971
Fredrik Svard <fredrik.svard@gmail.com>
2017-11-25 19:30:28 +0100

    correct

--- my_client.rb
+++ my_client.rb
@@ -27,6 +27,7 @@ class MyClient
      
     rescue => e
       puts 'got problem!!'
+      puts e
       2
   end
 
@@ -34,40 +35,3 @@ end
 
 
 
-if __FILE__ == $PROGRAM_NAME
-
-require 'minitest/autorun'
-require "minitest/reporters"
-Minitest::Reporters.use!
-
-
-  class Test_MyClient  < Minitest::Test
-
-  	def setup
-
-  		  @client = MyClient.new     
-    end
-
-  	def test_login
-  		VCR.use_cassette("mimgao") do
-   		@response = @client.get('http://example.com')
-   	    end
-   		refute_nil @response
-  	end
-
-
-    def test_failing_500
-      VCR.use_cassette("error500") do
-      @response = @client.get('https://httpstat.us/500')
-        end
-      refute_nil @response
-    end
-
-    def test_foaas
-    end
-   
-
-  end
-
-
-end
